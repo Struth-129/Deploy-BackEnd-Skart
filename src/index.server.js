a9f6e2d560cb60fc36caa675c8340bb36566d3e9
Struth129 <sakets.ug18.ee@gmail.com>
2021-03-28 23:17:40 +0530

    initial

---
+++ src/index.server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const env = require("dotenv");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+//routes
+const authRoutes = require("./routes/auth");
+const adminRoutes = require("./routes/admin/auth");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const cartRoutes = require("./routes/cart");
+const initialDataRoutes = require("./routes/admin/initialData");
+const pageRoutes = require("./routes/admin/page");
+const addressRoutes = require("./routes/address");
+const orderRoutes = require("./routes/order");
+const adminOrderRoute = require("./routes/admin/order.routes");
+
+//environment variable or you can say constants
+env.config();
+
+// mongodb connection
+//mongodb+srv://root:<password>@cluster0.8pl1w.mongodb.net/<dbname>?retryWrites=true&w=majority
+mongoose.connect(`mongodb+srv://struth:Saket@123@cluster0.b9wuc.mongodb.net/ecommerce?retryWrites=true&w=majority`,
+    {   useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useCreateIndex:true})
+        .then(()=>{
+            console.log("Connected");
+        }).catch((e)=>{
+            console.log(e);
+        })
+
+app.use(cors());
+app.use(express.json());
+app.use("/public", express.static(path.join(__dirname, "uploads")));
+app.use("/api", authRoutes);
+app.use("/api", adminRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", cartRoutes);
+app.use("/api", initialDataRoutes);
+app.use("/api", pageRoutes);
+app.use("/api", addressRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", adminOrderRoute);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
